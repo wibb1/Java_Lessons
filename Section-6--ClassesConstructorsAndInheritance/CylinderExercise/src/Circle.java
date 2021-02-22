c0787981275ec3be16e7a6d682dc7532143eacfe
Will Campbell <wibb1@comcast.net>
2021-02-22 13:18:27 -0500

    Cylinder Challenge

---
+++ Section-6--ClassesConstructorsAndInheritance/CylinderExercise/src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+  private double radius;
+
+  public Circle(double radius) {
+    this.radius = radius > 0 ? radius : 0;
+  }
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public double getArea() {
+    return getRadius() * getRadius() * Math.PI;
+  }
+}
---
+++ Section-6--ClassesConstructorsAndInheritance/CylinderExercise/src/Cylinder.java
@@ -0,0 +1,16 @@
+public class Cylinder extends Circle {
+  private double height;
+
+  public Cylinder(double radius, double height) {
+    super(radius);
+    this.height = height > 0 ? height : 0;
+  }
+
+  public double getHeight(){
+    return this.height;
+  }
+
+  public double getVolume(){
+    return this.getArea() * this.height;
+  }
+}
