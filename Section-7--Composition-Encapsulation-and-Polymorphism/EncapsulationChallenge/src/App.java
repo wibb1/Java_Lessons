08e24c95a93fe815e67a0acf792ddd39e65a7f90
Will Campbell <wibb1@comcast.net>
2021-02-22 19:42:13 -0500

    added duplex printing

--- Section-7--Composition-Encapsulation-and-Polymorphism/EncapsulationChallenge/src/App.java
+++ Section-7--Composition-Encapsulation-and-Polymorphism/EncapsulationChallenge/src/App.java
@@ -1,10 +1,17 @@
 public class App {
     public static void main(String[] args) {
         Printer printer1 = new Printer(false);
-        System.out.println("\n");
+        System.out.println("***********************");
         printer1.printPages(50);
         printer1.fillToner();
         printer1.printPages(10100);
-        System.out.println("\n");
+        System.out.println("***********************");
+
+        Printer printer2 = new Printer(true);
+        System.out.println("***********************");
+        printer2.printPages(50);
+        printer2.fillToner();
+        printer2.printPages(10100);
+        System.out.println("***********************");
     }
 }
--- Section-7--Composition-Encapsulation-and-Polymorphism/EncapsulationChallenge/src/Printer.java
+++ Section-7--Composition-Encapsulation-and-Polymorphism/EncapsulationChallenge/src/Printer.java
@@ -28,13 +28,14 @@ public class Printer {
 
   public void fillToner() {
     this.toner = 100;
-    System.out.println("\nToner has been filled to 100%.");
+    System.out.println("\nToner catridge replaced.");
     this.tonerAlert = false;
   }
 
   public void printPages(int pages) {
     int lessPages = 0;
     int printedPages = 0;
+
     if (useToner(pages) > this.toner) {
       printedPages = (int) (this.toner / 0.01);
       lessPages = pages - printedPages;
@@ -43,14 +44,19 @@ public class Printer {
     } else {
       this.toner -= useToner(pages);
     }
-    this.pages += pages;
+
+    if (duplex) {
+      pages = pages / 2 + pages % 2;
+      printedPages = printedPages / 2 + printedPages % 2;
+      lessPages = lessPages / 2 + lessPages % 2;
+    }
+
     if (this.isTonerAlert()) {
       printMessage(printedPages, lessPages);
-    } else if (isDuplex()) {
-      printMessage(pages);
     } else {
       printMessage(pages);
     }
+
   }
 
   public double useToner(int pages) {
@@ -59,16 +65,16 @@ public class Printer {
 
   public void printMessage(int pages) {
     duplexMessage();
-    System.out.println(pages + " pages printed, you have printed " + this.pages + " pages in total");
-    System.out.println("Your printer ink is " + this.toner + "% full.");
+    System.out.println(pages + " pages printed.");
+    System.out.println("You have printed " + this.pages + " pages in total");
+    System.out.println("Your toner cartridge is " + this.toner + "% full.");
   }
 
   public void printMessage(int pages, int lessPages) {
     duplexMessage();
-    System.out.println(pages + " pages printed, you have printed " + this.pages
-        + " pages in total, and your printer ink is empty.");
-    System.out.println("Please refill your printer ink to print the remaining "
-        + lessPages + " pages.");
+    System.out.println(pages + " pages printed");
+    System.out.println("You have printed " + this.pages + " pages in total.");
+    System.out.println("Your toner cartridge is empty, replace it to print the remaining " + lessPages + " pages.");
   }
 
   public void duplexMessage() {
@@ -77,5 +83,9 @@ public class Printer {
     } else {
       System.out.print("\nYou are printing single sided and have ");
     }
+  }
+
+  public int duplexPrint(int pages) {
+    return pages / 2 + pages % 2;
   }
 }
