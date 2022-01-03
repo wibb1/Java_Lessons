f795b1b98a22a8d3c8aa5690b605225f08559378
Will Campbell <wibb1@comcast.net>
2022-01-03 12:15:24 -0500

    Section 10 - Generics - Introduction

--- Section-10--JavaGenerics/Section-10--Introduction/src/Main.java
+++ Section-10--JavaGenerics/Section-10--Introduction/src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+//        This code works, but you are not protected from type entry
+//        ArrayList items = new ArrayList<>();
+//        items.add(1);
+//        items.add(2);
+//        items.add("This will cause an exception at runtime");
+//        items.add(3);
+//        items.add(4);
+//        items.add(5);
+
+
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        //items.add("This will cause an exception at runtime");
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+
+        printDoubled(items);
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (int i : items) {
+            System.out.println(i * 2);
+        }
+    }
 }
