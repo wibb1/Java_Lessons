e7f69d582739b6556b711e64d1ca6d92878f1cfb
Will Campbell <wibb1@comcast.net>
2022-02-05 07:07:50 -0500

    Adventure challenge end of 159

---
+++ Section-12--JavaCollections/Section-12--AdventureChallenge/src/com/willcampbell/Location.java
@@ -0,0 +1,33 @@
+package com.willcampbell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.description = description;
+        this.locationID = locationID;
+        this.exits = new HashMap<String, Integer>();
+        this.exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, Integer location) {
+        exits.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
---
+++ Section-12--JavaCollections/Section-12--AdventureChallenge/src/com/willcampbell/Main.java
@@ -0,0 +1,74 @@
+package com.willcampbell;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static Map<Integer, Location> locations = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(0, "You are standing at the end of a road before a small bridge"));
+        locations.put(2, new Location(0, "You are on top of a hill"));
+        locations.put(3, new Location(0, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(0, "You are in a valley beside a stream"));
+        locations.put(5, new Location(0, "You are in a forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+//        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
+        int loc = 1;
+//        while (true) {
+//            System.out.println(locations.get(loc).getDescription());
+//            if (loc == 0) {
+//                break;
+//            }
+//
+//            Map<String, Integer> exits = locations.get(loc).getExits();
+//            System.out.println("Available exits are ");
+//            for (String exit : exits.keySet()) {
+//                System.out.print(exit + ", ");
+//            }
+//            System.out.println();
+//
+//            String direction = scanner.nextLine().toUpperCase(Locale.ROOT);
+//
+//            if (exits.containsKey(direction)) {
+//                loc = exits.get(direction);
+//            } else {
+//                System.out.println("Tou cannot go in that direction");
+//            }
+//        }
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for (String i : road) {
+            System.out.println(i);
+        }
+
+        System.out.println("===================================================");
+
+        String[] building = "You are inside a building, a well house for a small spring".split(",");
+        for (String i : building) {
+            System.out.println(i);
+        }
+    }
+}
