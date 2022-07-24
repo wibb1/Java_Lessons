e294f9f178c1a2d9d674d57379b543cc34d0db62
Will Campbell <wibb1@comcast.net>
2022-07-24 12:29:15 -0400

    Section 13 - through lesson 188

---
+++ Section-13--JavaFX/Controls/src/main/java/com/example/controls/Main.java
@@ -0,0 +1,23 @@
+package com.example.controls;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Section-13--JavaFX/Controls/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.controls {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.controls to javafx.fxml;
+    exports com.example.controls;
+}
