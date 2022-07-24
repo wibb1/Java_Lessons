dec03d31bf8f101462f9fa8fcfede40aa2ac88f8
Will Campbell <wibb1@comcast.net>
2022-07-24 16:31:39 -0400

    Lesson 299

--- Section-16--LambdaExpressions/Employees/src/com/willcampbell/Main.java
+++ Section-16--LambdaExpressions/Employees/src/com/willcampbell/Main.java
@@ -3,6 +3,7 @@ package com.willcampbell;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -11,12 +12,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Employee john = new Employee("John", 15);
-        Employee tim = new Employee("Tim", 200);
-        Employee jack = new Employee("Jack", 12);
-        Employee jill = new Employee("Jill", 12);
-        Employee red = new Employee("Red", 18);
-        Employee charming = new Employee("Prince", 25);
+        Employee jack = new Employee("Jack Hill", 12);
+        Employee jill = new Employee("Jill Hill", 12);
+        Employee john = new Employee("John Paul", 15);
+        Employee tim = new Employee("Tim Buchalka", 200);
+        Employee red = new Employee("Red Riding-hood", 18);
+        Employee charming = new Employee("Prince Charming", 25);
 
         List<Employee> employees = new ArrayList<>();
         employees.add(jack);
@@ -25,33 +26,27 @@ public class Main {
         employees.add(tim);
         employees.add(red);
         employees.add(charming);
-        System.out.println("============================");
-        printEmployeesByAge(employees, "Employees over 15", employee -> employee.getAge() > 15);
-        System.out.println("============================");
-        printEmployeesByAge(employees, "Employees under 15", employee -> employee.getAge() < 15);
-        System.out.println("============================");
-        printEmployeesByAge(employees, "Employees younger than 100", new Predicate<Employee>() {
-            @Override
-            public boolean test(Employee employee) {
-                return employee.getAge() < 100;
+
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' ') + 1);
+        };
+
+        Random random1 = new Random();
+        for(Employee employee : employees) {
+            if(random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
             }
-        });
-        System.out.println("============================");
-
-        IntPredicate greaterThan15 = i -> i > 15;
-        IntPredicate lessThan100 = i -> i < 100;
-        System.out.println(greaterThan15.test(10));//false
-        int a = 20;
-        System.out.println(greaterThan15.test(a + 5));//true
-
-        System.out.println(greaterThan15.and(lessThan100).test(50)); // true
-        System.out.println(greaterThan15.and(lessThan100).test(5)); // false
-
-        Random random = new Random();
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-        for(int i=0; i<10 ; i++) {
-            System.out.println(randomSupplier.get());
         }
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
