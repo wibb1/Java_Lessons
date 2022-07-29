e9b40742955fe529e1854ca865aeabec9d62953d
Will Campbell <wibb1@comcast.net>
2022-07-28 20:24:25 -0400

    lesson 340

--- Section-19--Databases/TestDB/src/com/willcampbell/Main.java
+++ Section-19--Databases/TestDB/src/com/willcampbell/Main.java
@@ -1,8 +1,35 @@
 package com.willcampbell;
 
-public class Main {
+import java.sql.*;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+//        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\Coding\\databases\\testjava.db");
+//             Statement statement = conn.createStatement()){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\Coding\\databases\\testjava.db");
+//            conn.setAutoCommit(false); // turns off the autocommit feature
+            Statement statement = conn.createStatement();
+//            statement.execute("DROP TABLE contacts");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Tim', 6545678, 'tim@email.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Joe', 45632, 'joe@anywhere.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Jane', 4829484, 'jane@somewhere.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES('Fido', 9068, 'fido@email.com')");
+//            statement.execute("UPDATE contacts SET phone=5566789 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+            statement.execute("SELECT * FROM contacts");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name")
+                        + " " + resultSet.getString("phone")
+                        + " " + resultSet.getString("email"));
+            }
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 }
