d3b6c9a919ffb9b5d6bb6d9ee31683cfc20cbccc
Will Campbell <wibb1@comcast.net>
2022-11-05 13:17:54 -0400

    Section 237

--- Section-14--BasicInputAndOutput/I-O-Intro/src/Locations.java
+++ Section-14--BasicInputAndOutput/I-O-Intro/src/Locations.java
@@ -2,47 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
-    private static final Map<Integer, Location> locations = new HashMap<>();
+    private static final Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        try (FileWriter locFile = new FileWriter("locations.txt");
-             FileWriter dirFile = new FileWriter("directions.txt")) {
+        try (BufferedWriter locWriter = new BufferedWriter(new FileWriter("locations.txt"));
+             BufferedWriter dirWriter = new BufferedWriter(new FileWriter("directions.txt"))) {
             for (Location location : locations.values()) {
-                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+                locWriter.write(location.getLocationID() + ", " + location.getDescription() + "\n");
                 for (String direction : location.getExits().keySet()) {
-                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                    if(!direction.equalsIgnoreCase("Q"))
+                        dirWriter.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
                 }
             }
         }
     }
 
     static {
-        try (Scanner scanner= new Scanner(new FileReader("locations_big.txt"))){
-            scanner.useDelimiter(",");
-            while (scanner.hasNextLine()) {
-                int loc = scanner.nextInt();
-                scanner.skip(scanner.delimiter());
-                String desc = scanner.nextLine();
+        try (BufferedReader locReader = new BufferedReader(new FileReader("locations_big.txt"))) {
+            locReader.lines().forEach(line -> {
+                String[] data = line.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String desc = data[1];
                 Map<String, Integer> tempExit = new HashMap<>();
                 locations.put(loc, new Location(loc, desc, tempExit));
                 System.out.println("Imported loc: " + loc + "," + desc);
-            }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try (BufferedReader dirFile = new BufferedReader(new FileReader("directions_big.txt"))) {
-            String input = dirFile.readLine();
-            while (input != null) {
-                String[] data = input.split(",");
+        try (BufferedReader dirReader = new BufferedReader(new FileReader("directions_big.txt"))) {
+            dirReader.lines().forEach(line -> {
+                String[] data = line.split(",");
                 int loc = Integer.parseInt(data[0]);
                 String dir = data[1];
                 int dest = Integer.parseInt(data[2]);
                 System.out.println(loc + "," + dir + "," + dest);
                 Location location = locations.get(loc);
                 location.addExit(dir, dest);
-                input = dirFile.readLine();
-            }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
